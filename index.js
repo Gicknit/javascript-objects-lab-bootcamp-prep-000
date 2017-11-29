47671a91077fc0fc001378f6a654f07e4e639678
Nick Garcia <89nickgarcia@gmail.com>
2017-11-29 04:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,3 @@ function deleteFromObjectByKey(object, key){
   delete newObj[key];
   return newObj;
 }
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
-  return object;
-}
