e923052aa13a761b7178a83d2688c64eef7e0597
Nick Garcia <89nickgarcia@gmail.com>
2017-11-29 04:06:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
-  return object;
-}
-
 function deleteFromObjectByKey(object, key){
   const newObj = object.assign({}, object)
   delete newObj[key];
   return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
 }
